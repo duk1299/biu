02785f80072d34aa3f43c8993dfc6ca33a57f8ad
duk1299 <121307381+duk1299@users.noreply.github.com>
2023-02-13 20:56:04 +0800

    Update 51job.js

--- applications/51job.js
+++ applications/51job.js
@@ -7,6 +7,7 @@ var obj = JSON.parse($response.body);
 
 obj.resultbody.hotCompany = {"items":[]}
 obj.resultbody.banner = {"items":[]}
+obj.resultbody.serviceBanner = {"items":[]}
 
 $done({body:JSON.stringify(obj)});
 // 
