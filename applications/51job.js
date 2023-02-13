b662ed5159dcad9703bd64333109295fe924940f
duk1299 <121307381+duk1299@users.noreply.github.com>
2023-02-13 19:18:39 +0800

    Update 51job.js

--- applications/51job.js
+++ applications/51job.js
@@ -1,17 +1,11 @@
-/**
- * @fileoverview Example of HTTP rewrite of request header.
- *
- * @supported Quantumult X (v1.0.5-build188)
- *
- * [rewrite_local]
- * ^http://example\.com/resource9/ url script-request-header sample-rewrite-request-header.js
- */
+/*
+oh
+*/
 
-// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+var obj = JSON.parse($response.body); 
 
-var modifiedHeaders = $request.headers;
-modifiedHeaders['version'] = '12.7.5';
+obj["hotCompany"] = {"items":[]}
+obj["banner"] = {"items":[]}
 
-var modifiedPath = 'https://appapi.51job.com/api/util/get_version.php';
-
-$done({path: modifiedPath, headers : modifiedHeaders});
+$done({body:JSON.stringify(obj)});
+// 
