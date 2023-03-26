28a46ecfc4f322272b319f57c22da6feee684f83
duk1299 <121307381+duk1299@users.noreply.github.com>
2023-03-26 15:58:06 +0800

    Update ecovacs_ad.js

--- applications/ecovacs_ad.js
+++ applications/ecovacs_ad.js
@@ -1,14 +1,11 @@
 var obj = JSON.parse($response.body);
 
-// let newData = [];
-// let oriData = obj["data"];
-
 // for (let item of oriData) {
 //   if (item["iconType"] === "ROBOT" || item["iconType"] === "MINE") {
 //     newData.push(item);
 //   }
 // }
 
-obj["data"] = [];
+obj.data.bannerInfoList = [];
 
 $done({body:JSON.stringify(obj)});
